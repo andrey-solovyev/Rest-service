9ccbbcbde7a2625943c15127e1374d201f246228
andrey-solovyev <andru504176@gmail.com>
2020-03-04 19:57:45 +0300

    first coomit

---
+++ src/main/java/payroll/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+/*
+This interface, though empty on the surface, packs a punch given it supports:
+
+Creating new instances
+
+Updating existing ones
+
+Deleting
+
+Finding (one, all, by simple or complex properties)
+ */
+}
